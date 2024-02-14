c2a91a9e921589a28fc2711e6859201ebfc317a0
tommy galet <tommy.galet@gmail.com>
2024-02-14 17:58:00 +0100

    code en RMI, fait avec prof

--- src/Capteur.java
+++ src/Capteur.java
@@ -1,23 +1,28 @@
+import java.rmi.RemoteException;
+
 public class Capteur {
     public String codeUnique;
     public String coordonneesGPS;
+    public CentraleGestion centrale;
 
-    public Capteur(String codeUnique, String coordonneesGPS) {
+    public Capteur(String codeUnique, String coordonneesGPS, CentraleGestion centrale) {
         this.codeUnique = codeUnique;
         this.coordonneesGPS = coordonneesGPS;
+        this.centrale = centrale;
     }
 
-    public void demarrer(CentraleDeGestion centrale) {
-        centrale.ajouterCapteur(this);
+    public void demarrer() throws RemoteException {
+        centrale.ajouterCapteur(this.codeUnique);
         System.out.println("Capteur démarré : " + codeUnique);
     }
 
-    public void retirer(CentraleDeGestion centrale) {
-        centrale.retirerCapteur(this);
+    public void retirer() throws RemoteException {
+        centrale.retirerCapteur(this.codeUnique);
         System.out.println("Capteur retiré : " + codeUnique);
     }
 
-    public void mesurer(CentraleDeGestion centrale, double temperature, double humidite) {
-        centrale.enregistrerMesures(this, temperature, humidite);
+    public void mesurer(double temperature, double humidite) throws RemoteException {
+        centrale.enregistrerMesures(new DataCapteur(temperature, humidite, this.codeUnique));
+        //chose a imaginé a renvoyé au main pour tester plusieur fois la fonction
     }
 }
---
+++ src/DataCapteur.java
@@ -0,0 +1,16 @@
+public class DataCapteur implements java.io.Serializable{
+    public double temperature;
+    public double humidite;
+    public String codeUnique;
+
+    public DataCapteur(double temperature, double humidite, String codeUnique) {
+        this.temperature = temperature;
+        this.humidite = humidite;
+        this.codeUnique = codeUnique;
+    }
+
+    public String toString() {
+        return "Mesures du capteurs " + codeUnique + " - Température : " + temperature + ", Humidité : " + humidite;
+    }
+    
+}
